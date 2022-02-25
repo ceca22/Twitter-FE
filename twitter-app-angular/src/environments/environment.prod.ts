65490215fdc4b6f6b345d327f5abd9b672089eb8
Dejan Bogatinovski <bogatinovski.dejan@gmail.com>
2022-02-25 10:53:52 +0100

    Configure deployment scripts

--- twitter-app-angular/src/environments/environment.prod.ts
+++ twitter-app-angular/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl:"http://localhost:29391/api",
+  client_Id:"258997150931-f01upn19mj3oeattegscoo47elj68885.apps.googleusercontent.com",
 };
