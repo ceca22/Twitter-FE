3d30f6286f77d7c59b2363a6b4fe6092bcd80f51
ceca22 <78369030+ceca22@users.noreply.github.com>
2022-03-01 16:03:39 +0100

    t fe

--- twitter-app-angular/src/app/home-page/post-template/post-template.component.ts
+++ twitter-app-angular/src/app/home-page/post-template/post-template.component.ts
@@ -22,7 +22,7 @@ export class PostTemplateComponent implements OnInit{
 
   ngOnInit() {
     this.postDate = new Date(this.post.datePosted)
-    // console.log(this.post.datePosted)
+    console.log(this.post.datePosted);
     this.timeOfPost = this.getTime(this.postDate)
 	}
 
@@ -33,59 +33,61 @@ export class PostTemplateComponent implements OnInit{
           if(this.postDate.getDate()===this.todayDate.getDate()){
             if(this.postDate.getHours()===this.todayDate.getHours()){
               if(this.postDate.getMinutes()===this.todayDate.getMinutes()){
-                this.time = this.todayDate.getSeconds() - this.postDate.getSeconds()
-                return this.timeOfPost = this.time + "s"
+                        this.time = this.todayDate.getSeconds() - this.postDate.getSeconds()
+                        return this.timeOfPost = this.time + "s"
+              } else {
+                if(this.todayDate.getMinutes() > this.postDate.getMinutes() ){
+                  this.time = this.todayDate.getMinutes() + (60 - this.postDate.getSeconds())
+                  return this.timeOfPost = this.time + "m"
+                } else {
+                  this.time = (60 - this.postDate.getMinutes()) + this.todayDate.getMinutes()  
+                  return this.timeOfPost = this.time + "m"
+                }
               }
-              this.time = this.todayDate.getMinutes() - this.postDate.getMinutes()
-              return this.timeOfPost = this.time + "m"
-
-            }
-
-            if(this.postDate.getHours() !== this.todayDate.getHours()){
-              if(this.postDate.getMinutes()===this.todayDate.getMinutes()){
-                this.time = this.todayDate.getSeconds() - this.postDate.getSeconds()
-                return this.timeOfPost = this.time + "s"
-              } else if(this.postDate.getMinutes()!==this.todayDate.getMinutes()) {
-                this.time = this.todayDate.getMinutes() - this.postDate.getMinutes()
-                return this.timeOfPost = this.time + "m"
+            } 
+              this.time = this.todayDate.getHours() - this.postDate.getHours()
+              if(this.time === 1){
+                if(this.todayDate.getMinutes() > this.postDate.getMinutes() ){
+                  this.time = this.todayDate.getMinutes() + (60 - this.postDate.getMinutes())
+                  if(this.time > 59){
+                    return this.timeOfPost = 1 + "h"
+                  }
+                  return this.timeOfPost = this.time + "m"
+                } else {
+                  this.time = (60 - this.postDate.getMinutes()) + this.todayDate.getMinutes()  
+                  if(this.time > 59){
+                    return this.timeOfPost = 1 + "h"
+                  }
+                  return this.timeOfPost = this.time + "m"
+                }
               }
-              
-              
-            }
+              return this.timeOfPost = this.time + "h"
           }
-          if( this.todayDate.getDate() - this.postDate.getDate() === 1){
-            if(this.todayDate.getHours() < this.postDate.getHours()){
-              this.time = ((24 - this.postDate.getHours()) + this.todayDate.getHours() ) 
-              return this.timeOfPost = this.time + "h"
-            }
-            if(this.todayDate.getHours() > this.postDate.getHours()){
-              this.time = ((this.postDate.getHours()) - this.todayDate.getHours() ) 
-              if(this.time >= 23){
-              this.time = ((this.postDate.getHours() - this.todayDate.getHours()) *(-1) ) 
-              return this.timeOfPost = "1d"
+          if(this.postDate.getDate()!==this.todayDate.getDate()){
+            if(this.postDate.getDate() - this.todayDate.getDate() === 1){
+              this.time = this.todayDate.getHours() - this.postDate.getHours()
+              if(this.time > 23){
+                return this.timeOfPost = 1 + "d"
+                
               }
               return this.timeOfPost = this.time + "h"
+
             }
             
+              return this.timeOfPost = this.postDate.toLocaleString('en-us', { month: 'short' }) 
+              + ", " + this.postDate.getDate()
+            
+            
           }
-  
+
         }
-        return this.timeOfPost = this.postDate.toLocaleString('en-us', { month: 'short' }) + ", " + 
-        this.postDate.getDate()
-        }
-        return this.timeOfPost = this.postDate.toLocaleString('en-us', { month: 'short' }) + ", " + 
-        this.postDate.getDate()
+        return this.timeOfPost = this.postDate.toLocaleString('en-us', { month: 'short' }) 
+              + ", " + this.postDate.getDate()
+      }
+      return this.timeOfPost = this.postDate.toLocaleString('en-us', { month: 'short' }) 
+              + ", " + this.postDate.getDate()
     }
-    
     return null;
   }
-    
-}
-    
 
-     
-    
-    
-  
-  
- 
+}
